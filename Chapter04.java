cbd2b2d7fb7c6884d7b0e44034ecb4a798c16f07
kazu5470 <105265063+kazu5470@users.noreply.github.com>
2022-10-08 13:34:19 +0000

    Chapter04

---
+++ Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        String str1;
+        str1 = "小笠原　和磨";
+        System.out.println(str1);
+        str1 = "こんにちは";
+        final int int1 = 7;
+        System.out.println(str1);
+        System.out.println(int1);
+    }
+}
